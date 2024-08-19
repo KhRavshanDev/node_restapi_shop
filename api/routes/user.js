c7704cacc9d7d7115f8be5f180f4eb207b36d8d8
KhRavshanDev <ravshanxakimov2002@gmail.com>
2024-08-19 15:09:06 +0500

    feat: Add login route for user authentication
    The code changes include:
    - Implementing the `/login` route in the `api/routes/user.js` file to handle user authentication
    
    This commit message follows the convention of starting with a type (`feat` for a new feature) and providing a concise and descriptive message about the changes made.

--- api/routes/user.js
+++ api/routes/user.js
@@ -44,6 +44,33 @@ router.post("/signup", (req, res, next) => {
     })
 });
 
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+        if(user.length < 1) {
+            return res.status(401).json({
+                message: 'Auth failed'
+            });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if(err) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            if (result) {
+                return res.status(200).json({
+                    message: 'Auth succesful'
+                });
+            }
+            res.status(401).json({
+                message: 'Auth failed'
+            });
+        });
+    })
+})
+
 router.delete("/:userId", (req, res, next) => {
     User.findByIdAndDelete({ _id: req.params.userId })
     .exec()
