5c672bec944cf7919e89c8b8c672fc726990ed4a
KhRavshanDev <ravshanxakimov2002@gmail.com>
2024-08-20 16:28:22 +0500

    refactor: Implement deleteUser function in UserController
    The code changes include:
    - Adding the `deleteUser` function in the `UserController` to handle the deletion of a user by their ID
    - Updating the `/api/routes/user.js` file to use the `UserController.deleteUser` function for the `/userId` DELETE route
    
    This commit message follows the convention of starting with a type (`refactor` for code refactoring) and providing a concise and descriptive message about the changes made.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -79,3 +79,19 @@ exports.login = (req, res, next) => {
         });
     })
 };
+
+exports.deleteUser = (req, res, next) => {
+    User.findByIdAndDelete({ _id: req.params.userId })
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message: 'User deleted',
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+}
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,30 +1,11 @@
 require('dotenv').config();
 const express = require("express");
 const router = express();
-const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
-const User = require("../models/user.js")
 const UserController = require("../controllers/user.js")
 
 router.post("/signup", UserController.register);
 router.post('/login', UserController.login);
-
-router.delete("/:userId", (req, res, next) => {
-    User.findByIdAndDelete({ _id: req.params.userId })
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            message: 'User deleted',
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.delete("/:userId", UserController.deleteUser);
 
 module.exports = router;
