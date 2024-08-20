baccd562c045d74155b8c54710ab12cfa88e2163
KhRavshanDev <ravshanxakimov2002@gmail.com>
2024-08-21 00:22:55 +0500

    refactor: Add checkAuth middleware to deleteUser route

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require("express");
 const router = express();
+const checkAuth = require("../middleware/check-auth.js");
 
 const UserController = require("../controllers/user.js")
 
 router.post("/signup", UserController.register);
 router.post('/login', UserController.login);
-router.delete("/:userId", UserController.deleteUser);
+router.delete("/:userId", checkAuth, UserController.deleteUser);
 
 module.exports = router;
