5894c142275e1de9a06ad1e5066835824dccd35d
KhRavshanDev <ravshanxakimov2002@gmail.com>
2024-08-20 16:14:35 +0500

    refactor: Update product routes to use controller functions

--- api/routes/product.js
+++ api/routes/product.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 const checkAuth = require('../middleware/check-auth.js');
 
+const ProductsController = require('../controllers/product.js')
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads/");
@@ -29,138 +31,10 @@ const upload = multer({
   fileFilter: fileFilter,
 });
 
-const Product = require("../models/product.js");
-
-router.get("/", (req, res, next) => {
-  Product.find()
-    .select("name price _id productImage")
-    .exec()
-    .then((docs) => {
-      const response = {
-        count: docs.length,
-        products: docs.map((doc) => {
-          return {
-            name: doc.name,
-            price: doc.price,
-            productImage: doc.productImage,
-            _id: doc._id,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/products/" + doc._id,
-            },
-          };
-        }),
-      };
-      res.status(200).json(response);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
-
-router.post("/", checkAuth, upload.single("productImage"), (req, res, next) => {
-  console.log(req.file);
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price,
-    productImage: req.file.path
-  });
-  product
-    .save()
-    .then((result) => {
-      res.status(201).json({
-        message: "Product created succesfully",
-        createdProduct: {
-          name: result.name,
-          price: result.price,
-          _id: result._id,
-          request: {
-            type: "POST",
-            url: "http://localhost:3000/products/" + result._id,
-          },
-        },
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
-
-router.get("/:productId", (req, res, next) => {
-  const id = req.params.productId;
-  Product.findById(id)
-    .select("name price _id productImage")
-    .exec()
-    .then((doc) => {
-      console.log(doc);
-      if (doc) {
-        res.status(200).json({
-          product: doc,
-          request: {
-            type: "GET",
-            url: "http://localhost:3000/products/" + doc._id,
-          },
-        });
-      } else {
-        res.status(404).json({ message: "Not found" });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-}),
-  router.put("/:productId", checkAuth, (req, res, next) => {
-    const id = req.params.productId;
-    const updateOps = {};
-    for (const ops of req.body) {
-      updateOps[ops.propName] = ops.value;
-    }
-    Product.findByIdAndUpdate({ _id: id }, { $set: updateOps })
-      .exec()
-      .then((result) => {
-        res.status(200).json({
-          message: "Product updated succesfully",
-          request: {
-            type: "PUT",
-            url: "http://localhost:3000/products/" + id,
-          },
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({
-          error: err,
-        });
-      });
-  });
-
-router.delete("/:productId", checkAuth, (req, res, next) => {
-  const id = req.params.productId;
-  Product.findByIdAndDelete({ _id: id })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "Product deleted",
-        request: {
-          type: "POST",
-          url: "http://localhost:3000/products",
-          body: { name: "String", price: "String" },
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.get("/", ProductsController.getAllProducts);
+router.post("/", checkAuth, upload.single("productImage"), ProductsController.createProduct);
+router.get("/:productId", ProductsController.getProduct);
+router.put("/:productId", checkAuth, ProductsController.updateProduct);
+router.delete("/:productId", checkAuth, ProductsController.deleteProduct);
 
 module.exports = router;
